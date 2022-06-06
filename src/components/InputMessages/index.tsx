7f217d1ec026621460bce7aff1eae8efbf18edb2
tomidamianovich <tomasdamianovich@gmail.com>
2022-06-06 18:13:18 -0300

    adding husky

--- src/components/InputMessages/index.tsx
+++ src/components/InputMessages/index.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import { BalanceType, TransactionActionType, BalanceWithCurrencyInfo } from '../../utils/type';
-import { TRANSACTIONS } from '../../utils/constants';
-import { withTranslation, WithTranslation } from 'react-i18next';
-import './styles/index.scss';
+import React from 'react'
+import {
+  BalanceType,
+  TransactionActionType,
+  BalanceWithCurrencyInfo,
+} from '../../utils/type'
+import { TRANSACTIONS } from '../../utils/constants'
+import { withTranslation, WithTranslation } from 'react-i18next'
+import './styles/index.scss'
 
 type Props = {
-  transactionWipCurrency: BalanceWithCurrencyInfo,
-  transactionAmount: string,
-  transactionType: TransactionActionType,
+  transactionWipCurrency: BalanceWithCurrencyInfo
+  transactionAmount: string
+  transactionType: TransactionActionType
   currentBalance: BalanceType
-} & WithTranslation;
+} & WithTranslation
 
 /*
 
@@ -25,50 +29,68 @@ const InputMessages: React.FC<Props> = ({
   transactionAmount,
   transactionType,
   currentBalance,
-  t
+  t,
 }) => {
-  const { sell_rate, account_limit } = transactionWipCurrency;
-  const isLimitedExceeded = account_limit && parseFloat(transactionAmount) > account_limit;
-  const isTransactionAffordable = account_limit && parseFloat(transactionAmount) * parseFloat(sell_rate) > currentBalance.amount;
-  const isBuyTransaction = transactionType === TRANSACTIONS.TYPES.BUY;
-  
-  const getTransactionAmountFormatted = (transactionAmount: string, sellRate:string) => {
-    const value = parseFloat(transactionAmount) * parseFloat(sellRate);
-    return value.toFixed(2);
+  const { sell_rate, account_limit } = transactionWipCurrency
+  const isLimitedExceeded =
+    account_limit && parseFloat(transactionAmount) > account_limit
+  const isTransactionAffordable =
+    account_limit &&
+    parseFloat(transactionAmount) * parseFloat(sell_rate) >
+      currentBalance.amount
+  const isBuyTransaction = transactionType === TRANSACTIONS.TYPES.BUY
+
+  const getTransactionAmountFormatted = (
+    transactionAmount: string,
+    sellRate: string,
+  ) => {
+    const value = parseFloat(transactionAmount) * parseFloat(sellRate)
+    return value.toFixed(2)
   }
 
-  const total = isBuyTransaction 
+  const total = isBuyTransaction
     ? getTransactionAmountFormatted(transactionAmount, sell_rate)
-    : transactionAmount;
+    : transactionAmount
 
   return (
     <div className="fe-wallet__input__messages">
-      { isLimitedExceeded && !isTransactionAffordable &&
+      {isLimitedExceeded && !isTransactionAffordable && (
         <span className="fe-wallet__input__messages__text--warning">
-          {isBuyTransaction ? "Limite excedido" : "Monto no disponible"}
-        </span> 
-      }
-      { isTransactionAffordable && isBuyTransaction &&
+          {isBuyTransaction ? 'Limite excedido' : 'Monto no disponible'}
+        </span>
+      )}
+      {isTransactionAffordable && isBuyTransaction && (
         <span className="fe-wallet__input__messages__text--warning">
           {t('balance.detail.input.labels.messages.insufficient_amount')}
-        </span> 
-      }
-      { isBuyTransaction && sell_rate && currentBalance?.ticker && transactionAmount && !isTransactionAffordable &&
+        </span>
+      )}
+      {isBuyTransaction &&
+        sell_rate &&
+        currentBalance?.ticker &&
+        transactionAmount &&
+        !isTransactionAffordable && (
+          <p className="fe-wallet__input__messages__text">
+            {t('balance.detail.input.labels.messages.amount_to_debit')}: $
+            {currentBalance?.ticker} {total}
+          </p>
+        )}
+      {isBuyTransaction &&
+        sell_rate &&
+        currentBalance?.ticker &&
+        transactionAmount &&
+        isTransactionAffordable && (
+          <p className="fe-wallet__input__messages__text">
+            {t('balance.detail.input.labels.messages.amount_needed')}:{' '}
+            {isBuyTransaction ? currentBalance?.ticker : ''} {total}
+          </p>
+        )}
+      {transactionWipCurrency?.amount && (
         <p className="fe-wallet__input__messages__text">
-          {t('balance.detail.input.labels.messages.amount_to_debit')}: ${currentBalance?.ticker} {total}
-        </p> 
-      }
-      { isBuyTransaction && sell_rate && currentBalance?.ticker && transactionAmount && isTransactionAffordable &&
-        <p className="fe-wallet__input__messages__text">
-          {t('balance.detail.input.labels.messages.amount_needed')}: { isBuyTransaction ? currentBalance?.ticker : ""} {total}
-        </p> 
-      }
-      {  transactionWipCurrency?.amount &&
-        <p className="fe-wallet__input__messages__text">
-          {t('balance.detail.input.labels.messages.actual_balance')}: {transactionWipCurrency?.ticker} {transactionWipCurrency?.amount}
-        </p> 
-      }
+          {t('balance.detail.input.labels.messages.actual_balance')}:{' '}
+          {transactionWipCurrency?.ticker} {transactionWipCurrency?.amount}
+        </p>
+      )}
     </div>
   )
 }
-export default withTranslation()(InputMessages);
+export default withTranslation()(InputMessages)
